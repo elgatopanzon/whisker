ef349363b7ea93783b8937a3d05f25b525985b7c
ElGatoPanzon <gato@elgatopanzon.io>
2025-02-14 19:16:20 -0600

    whisker_debug.h with initial function to print object representation in bytes

---
+++ src/whisker_debug.c
@@ -0,0 +1,19 @@
+/**
+ * @author      : ElGatoPanzon (contact@elgatopanzon.io)
+ * @file        : whisker_debug
+ * @created     : Friday Feb 14, 2025 19:14:40 CST
+ */
+
+#include "whisker_std.h"
+
+#include "whisker_debug.h"
+
+// take an object and it's size, print the bytes representation
+void whisker_debug_print_value_layout(void *obj, size_t len, const char *name)
+{
+	unsigned char *bytes = (unsigned char *) obj;
+	printf("%s representation: 0x", name);
+	while (len--)
+		printf("%02x", *bytes++);
+	printf("\n");
+}
---
+++ src/whisker_debug.h
@@ -0,0 +1,15 @@
+/**
+ * @author      : ElGatoPanzon (contact@elgatopanzon.io)
+ * @file        : whisker_debug
+ * @created     : Friday Feb 14, 2025 19:13:56 CST
+ */
+
+#include "whisker_std.h"
+
+#ifndef WHISKER_DEBUG_H
+#define WHISKER_DEBUG_H
+
+void whisker_debug_print_value_layout(void *obj, size_t len, const char *name);
+
+#endif /* WHISKER_DEBUG_H */
+
