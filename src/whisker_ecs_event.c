159153432f7c9df2dcc9d00c0f37dfbd2b186a79
ElGatoPanzon <gato@elgatopanzon.io>
2025-03-29 11:53:05 -0600

    ecs_event: update functions to take an entity pool instead of entities and components structs

--- src/whisker_ecs_event.c
+++ src/whisker_ecs_event.c
@@ -9,77 +9,76 @@
 #include "whisker_ecs_event.h"
 
 // create an entity to represent an event
-whisker_ecs_entity_id whisker_ecs_ev_create_event(whisker_ecs_entities *entities, whisker_ecs_components *components)
+whisker_ecs_entity_id whisker_ecs_ev_create_event(whisker_ecs_pool *pool)
 {
-	whisker_ecs_entity_id ev = whisker_ecs_e_create(entities);
+	whisker_ecs_entity_id ev = whisker_ecs_p_request_entity(pool);
 
-	// set the entity to dead and add it to the deferred entities
-	entities->entities[ev.index].destroyed = true;
-	whisker_ecs_e_add_deffered_action(entities, (whisker_ecs_entity_deferred_action){.id = ev, .action = WHISKER_ECS_ENTITY_DEFERRED_ACTION_CREATE});
+	pool->entities->entities[ev.index].destroyed = true;
+	whisker_ecs_e_add_deffered_action(pool->entities, (whisker_ecs_entity_deferred_action){.id = ev, .action = WHISKER_ECS_ENTITY_DEFERRED_ACTION_CREATE});
 
 	// set the t_event component on the entity
-	whisker_ecs_ev_set_data_f(entities, components, ev, whisker_ecs_e_create_named(entities, "t_event"), 0, NULL);
+	whisker_ecs_ev_set_data_f(pool, ev, whisker_ecs_e_create_named(pool->entities, "t_event"), sizeof(bool), &(bool){0});
 
 	// make the entity unmanaged
-	whisker_ecs_e_make_unmanaged(entities, ev);
+	whisker_ecs_e_make_unmanaged(pool->entities, ev);
 
 	return ev;
 }
 
 // create an event without attached data
-whisker_ecs_entity_id whisker_ecs_ev_create_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id)
+whisker_ecs_entity_id whisker_ecs_ev_create_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id)
 {
-	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(entities, components);
-	whisker_ecs_ev_set_data_f(entities, components, ev, event_component_id, 0, NULL);
+	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(pool);
+	whisker_ecs_ev_set_data_f(pool, ev, event_component_id, 0, &(bool){0});
 	return ev;
 }
 
 // create an event with attached data
-whisker_ecs_entity_id whisker_ecs_ev_create_with_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data)
+whisker_ecs_entity_id whisker_ecs_ev_create_with_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data)
 {
-	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(entities, components);
-	whisker_ecs_ev_set_data_f(entities, components, ev, event_component_id, component_size, event_data);
+	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(pool);
+	whisker_ecs_ev_set_data_f(pool, ev, event_component_id, component_size, event_data);
 	return ev;
 }
 
 // create an event entity and immediately fire it
-void whisker_ecs_ev_create_and_fire_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id)
+void whisker_ecs_ev_create_and_fire_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id)
 {
-	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(entities, components);
-	whisker_ecs_ev_set_data_f(entities, components, ev, event_component_id, 0, NULL);
-	whisker_ecs_ev_fire(entities, ev);
+	whisker_ecs_entity_id ev = whisker_ecs_ev_create_event(pool);
+	whisker_ecs_ev_set_data_f(pool, ev, event_component_id, sizeof(bool), &(bool){0});
+	whisker_ecs_ev_fire(pool, ev);
 }
 
 // set component data on an event entity
-void whisker_ecs_ev_set_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_entity_id, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data)
+void whisker_ecs_ev_set_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_entity_id, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data)
 {
-	whisker_ecs_c_create_deferred_action(components, event_component_id, event_entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_SET, event_data, component_size);
+	whisker_ecs_c_create_deferred_action(pool->components, event_component_id, event_entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_SET, event_data, component_size, false);
 }
 
 // fire off an event entity
-void whisker_ecs_ev_fire(whisker_ecs_entities *entities, whisker_ecs_entity_id event_entity_id)
+void whisker_ecs_ev_fire(whisker_ecs_pool *pool, whisker_ecs_entity_id event_entity_id)
 {
 	// firing an event is just making it managed again
-	whisker_ecs_e_make_managed(entities, event_entity_id);
+	whisker_ecs_e_make_managed(pool->entities, event_entity_id);
 }
 
 // fire an event, attaching it instead to the given entity ID
-void whisker_ecs_ev_fire_on_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, whisker_ecs_entity_id fire_on_entity_id)
+void whisker_ecs_ev_fire_on_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, whisker_ecs_entity_id fire_on_entity_id)
 {
 	// add the event to the provided entity ID
-	whisker_ecs_ev_set_fire_on_data_f(entities, components, event_component_id, 0, NULL, fire_on_entity_id);
+	whisker_ecs_ev_set_fire_on_data_f(pool, event_component_id, sizeof(bool), &(bool){0}, fire_on_entity_id);
 }
 
 // fire an event, attaching it with data to the given entity ID
-void whisker_ecs_ev_fire_on_with_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id)
+void whisker_ecs_ev_fire_on_with_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id)
 {
-	whisker_ecs_ev_set_fire_on_data_f(entities, components, event_component_id, component_size, event_data, fire_on_entity_id);
+	whisker_ecs_ev_set_fire_on_data_f(pool, event_component_id, component_size, event_data, fire_on_entity_id);
 }
 
 // set data on an entity, will be removed along with the event
-void whisker_ecs_ev_set_fire_on_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id)
+void whisker_ecs_ev_set_fire_on_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id)
 {
-	whisker_ecs_ev_set_data_f(entities, components, fire_on_entity_id, event_component_id, component_size, event_data);
+	whisker_ecs_ev_set_data_f(pool, fire_on_entity_id, event_component_id, component_size, event_data);
 
 	// make sure the event data is culled for this event
 	struct whisker_ecs_event_cull_event_component cull_component = {
@@ -88,8 +87,8 @@ void whisker_ecs_ev_set_fire_on_data_f(whisker_ecs_entities *entities, whisker_e
 	};
 
 	// create a data cull event for this entity to remove the fire_on data
-	whisker_ecs_entity_id cull_ev = whisker_ecs_ev_create_with_data_f(entities, components, whisker_ecs_e_create_named(entities, "t_event_cull_data"), sizeof(cull_component), &cull_component);
-	whisker_ecs_ev_fire(entities, cull_ev);
+	whisker_ecs_entity_id cull_ev = whisker_ecs_ev_create_with_data_f(pool, whisker_ecs_e_create_named(pool->entities, "t_event_cull_data"), sizeof(cull_component), &cull_component);
+	whisker_ecs_ev_fire(pool, cull_ev);
 }
 
 /*****************
@@ -104,7 +103,9 @@ void whisker_ecs_ev_system_cull_events(whisker_ecs_system_context *context)
 	whisker_ecs_system_iterator *itor = whisker_ecs_s_get_iterator(context, 0, "t_event", "", "");
 	while (whisker_ecs_s_iterate(itor)) 
 	{
-		debug_log(DEBUG, ecs:system_cull_events, "event: culling event entity %zu", itor->entity_id.id);
+		/* debug_log(DEBUG, ecs:system_cull_events, "event: culling event entity %zu", itor->entity_id.id); */
+		whisker_ecs_c_create_deferred_action(context->components, itor->component_ids_rw[0], itor->entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_REMOVE, NULL, 0, false);
+
 		whisker_ecs_e_add_deffered_action(context->entities, (whisker_ecs_entity_deferred_action){.id = itor->entity_id, .action = WHISKER_ECS_ENTITY_DEFERRED_ACTION_DESTROY});
 	}
 }
@@ -122,8 +123,12 @@ void whisker_ecs_ev_system_cull_event_components(whisker_ecs_system_context *con
 		// issue the deferred component removal request
 		struct whisker_ecs_event_cull_event_component *cull = whisker_ecs_itor_get(itor, 0);
 
-		debug_log(DEBUG, ecs:system_cull_events, "event: culling fire_on component %zu from entity %zu", cull->component_id.id, cull->entity_id.id);
+		/* debug_log(DEBUG, ecs:system_cull_events, "event: culling fire_on component %zu from entity %zu", cull->component_id.id, cull->entity_id.id); */
 
-		whisker_ecs_c_create_deferred_action(context->components, cull->component_id, cull->entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_REMOVE, NULL, 0);
+		if (cull->component_id.id > 0 && cull->entity_id.id > 0)
+		{
+			whisker_ecs_c_create_deferred_action(context->components, cull->component_id, cull->entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_REMOVE, NULL, 0, false);
+		}
+		whisker_ecs_c_create_deferred_action(context->components, itor->component_ids_rw[0], itor->entity_id, WHISKER_ECS_COMPONENT_DEFERRED_ACTION_REMOVE, NULL, 0, false);
 	}
 }
--- src/whisker_ecs_event.h
+++ src/whisker_ecs_event.h
@@ -8,79 +8,80 @@
 #include "whisker_ecs_entity.h"
 #include "whisker_ecs_component.h"
 #include "whisker_ecs_system.h"
+#include "whisker_ecs_pool.h"
 
 #ifndef WHISKER_ECS_EVENT_H
 #define WHISKER_ECS_EVENT_H
 
 // macros accepting named components
-#define whisker_ecs_ev_create(e, c, id) \
-	whisker_ecs_ev_create_f(e, c, id);\
+#define whisker_ecs_ev_create(p, id) \
+	whisker_ecs_ev_create_f(p, id);\
 	debug_log(DEBUG, ecs:event_create, "creating event %zu", id.id);
 
-#define whisker_ecs_ev_create_named(e, c, n) \
-	whisker_ecs_ev_create_f(e, c, whisker_ecs_e_create_named(e, #n)); \
+#define whisker_ecs_ev_create_named(p, n) \
+	whisker_ecs_ev_create_f(p, whisker_ecs_e_create_named(e, #n)); \
 	debug_log(DEBUG, ecs:event_create, "creating event %s", #n);
 
-#define whisker_ecs_ev_create_with_data(e, c, id, t, v) \
-	whisker_ecs_ev_create_with_data_f(e, c, id, sizeof(t), v); \
+#define whisker_ecs_ev_create_with_data(p, id, t, v) \
+	whisker_ecs_ev_create_with_data_f(p, id, sizeof(t), v); \
 	debug_log(DEBUG, ecs:event_create, "creating event %zu with data type %s", id.id, #t);
 
-#define whisker_ecs_ev_create_with_data_named(e, c, n, t, v) \
-	whisker_ecs_ev_create_with_data_f(e, c, whisker_ecs_e_create_named(e, #n), sizeof(t), v); \
+#define whisker_ecs_ev_create_with_data_named(p, n, t, v) \
+	whisker_ecs_ev_create_with_data_f(p, whisker_ecs_e_create_named(e, #n), sizeof(t), v); \
 	debug_log(DEBUG, ecs:event_create, "creating event %s with data type %s", #n, #t);
 
-#define whisker_ecs_ev_create_and_fire(e, c, id) \
-	whisker_ecs_ev_create_and_fire_f(e, c, id); \
+#define whisker_ecs_ev_create_and_fire(p, id) \
+	whisker_ecs_ev_create_and_fire_f(p, id); \
 	debug_log(DEBUG, ecs:event_create, "creating event %zu and firing", id.id);
 
-#define whisker_ecs_ev_create_and_fire_named(e, c, n) \
-	whisker_ecs_ev_create_and_fire_f(e, c, whisker_ecs_e_create_named(e, #n)); \
+#define whisker_ecs_ev_create_and_fire_named(p, n) \
+	whisker_ecs_ev_create_and_fire_f(p, whisker_ecs_e_create_named(e, #n)); \
 	debug_log(DEBUG, ecs:event_create, "creating event %s and firing", #n);
 
 
-#define whisker_ecs_ev_set_data(e, c, ev, id, t, v) \
-	whisker_ecs_ev_set_data_f(e, c, ev, id, sizeof(t), v); \
+#define whisker_ecs_ev_set_data(p, ev, id, t, v) \
+	whisker_ecs_ev_set_data_f(p, ev, id, sizeof(t), v); \
 	debug_log(DEBUG, ecs:event_create, "setting event %zu data with data type %zu", id.id, #t);
 
-#define whisker_ecs_ev_set_data_named(e, c, ev, n, t, v) \
-	whisker_ecs_ev_set_data_f(e, c, ev, whisker_ecs_e_create_named(e, #n), sizeof(t), v); \
+#define whisker_ecs_ev_set_data_named(p, ev, n, t, v) \
+	whisker_ecs_ev_set_data_f(p, ev, whisker_ecs_e_create_named(e, #n), sizeof(t), v); \
 	debug_log(DEBUG, ecs:event_create, "setting event %s data with data type %s", #n, #t);
 
 
-#define whisker_ecs_ev_fire_on(e, c, id, on) \
-	whisker_ecs_ev_fire_on_f(e, c, id, on); \
+#define whisker_ecs_ev_fire_on(p, id, on) \
+	whisker_ecs_ev_fire_on_f(p, id, on); \
 	debug_log(DEBUG, ecs:event_create, "creating event %zu and firing on %zu", id.id, on.id);
 
-#define whisker_ecs_ev_fire_on_named(e, c, n, on) \
-	whisker_ecs_ev_fire_on_f(e, c, whisker_ecs_e_create_named(e, #n), on); \
+#define whisker_ecs_ev_fire_on_named(p, n, on) \
+	whisker_ecs_ev_fire_on_f(p, whisker_ecs_e_create_named(e, #n), on); \
 	debug_log(DEBUG, ecs:event_create, "creating event %s and firing on %zu", #n, on.id);
 
-#define whisker_ecs_ev_fire_on_with_data(e, c, id, t, v, on) \
-	whisker_ecs_ev_fire_on_with_data_f(e, c, id, sizeof(t), v, on); \
+#define whisker_ecs_ev_fire_on_with_data(p, id, t, v, on) \
+	whisker_ecs_ev_fire_on_with_data_f(p, id, sizeof(t), v, on); \
 	debug_log(DEBUG, ecs:event_create, "creating event %zu and firing on %zu with data type %s", id.id, on.id, #t);
 
-#define whisker_ecs_ev_fire_on_with_data_named(e, c, n, t, v, on) \
-	whisker_ecs_ev_fire_on_with_data_f(e, c, whisker_ecs_e_create_named(e, #n), sizeof(t), v, on); \
+#define whisker_ecs_ev_fire_on_with_data_named(p, n, t, v, on) \
+	whisker_ecs_ev_fire_on_with_data_f(p, whisker_ecs_e_create_named(e, #n), sizeof(t), v, on); \
 	debug_log(DEBUG, ecs:event_create, "creating event %s and firing on %zu with data type %s", #n, on.id, #t);
 
-#define whisker_ecs_ev_set_fire_on_data(e, c, id, t, v, on) \
-	whisker_ecs_ev_set_fire_on_data_f(e, c, id, sizeof(t), v, on); \
+#define whisker_ecs_ev_set_fire_on_data(p, id, t, v, on) \
+	whisker_ecs_ev_set_fire_on_data_f(p, id, sizeof(t), v, on); \
 	debug_log(DEBUG, ecs:event_create, "setting fire on event %zu on %zu with data type %s", id.id, on.id, #t);
 
-#define whisker_ecs_ev_set_fire_on_data_named(e, c, n, t, v, on) \
-	whisker_ecs_ev_set_fire_on_data_f(e, c, whisker_ecs_e_create_named(e, #n), sizeof(t), v, on); \
+#define whisker_ecs_ev_set_fire_on_data_named(p, n, t, v, on) \
+	whisker_ecs_ev_set_fire_on_data_f(p, whisker_ecs_e_create_named(e, #n), sizeof(t), v, on); \
 	debug_log(DEBUG, ecs:event_create, "setting fire on event %s on %zu with data type %s", #n, on.id, #t);
 
 // event management functions
-whisker_ecs_entity_id whisker_ecs_ev_create_event(whisker_ecs_entities *entities, whisker_ecs_components *components);
-whisker_ecs_entity_id whisker_ecs_ev_create_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id);
-whisker_ecs_entity_id whisker_ecs_ev_create_with_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data);
-void whisker_ecs_ev_create_and_fire_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id);
-void whisker_ecs_ev_set_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_entity_id, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data);
-void whisker_ecs_ev_fire(whisker_ecs_entities *entities, whisker_ecs_entity_id event_entity_id);
-void whisker_ecs_ev_fire_on_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, whisker_ecs_entity_id fire_on_entity_id);
-void whisker_ecs_ev_fire_on_with_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id);
-void whisker_ecs_ev_set_fire_on_data_f(whisker_ecs_entities *entities, whisker_ecs_components *components, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id);
+whisker_ecs_entity_id whisker_ecs_ev_create_event(whisker_ecs_pool *pool);
+whisker_ecs_entity_id whisker_ecs_ev_create_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id);
+whisker_ecs_entity_id whisker_ecs_ev_create_with_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data);
+void whisker_ecs_ev_create_and_fire_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id);
+void whisker_ecs_ev_set_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_entity_id, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data);
+void whisker_ecs_ev_fire(whisker_ecs_pool *pool, whisker_ecs_entity_id event_entity_id);
+void whisker_ecs_ev_fire_on_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, whisker_ecs_entity_id fire_on_entity_id);
+void whisker_ecs_ev_fire_on_with_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id);
+void whisker_ecs_ev_set_fire_on_data_f(whisker_ecs_pool *pool, whisker_ecs_entity_id event_component_id, size_t component_size, void *event_data, whisker_ecs_entity_id fire_on_entity_id);
 
 // ECS components and systems to manage events
 struct whisker_ecs_event_cull_event_component 
