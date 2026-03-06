867ce848b790fe3b0f92c9175e4f01cd347e6aba
ElGatoPanzon <gato@elgatopanzon.io>
2026-03-05 19:49:40 -0600

    bench: world: buffered vs unbuffered benchmarks

--- benchmarks/benchmark_whisker_ecs_world.c
+++ benchmarks/benchmark_whisker_ecs_world.c
@@ -233,4 +233,320 @@ UBENCH_F(bench_update_10_systems, 10_phases)
 }
 
 
+// ============================================================================
+// create entities: world API vs direct registry
+// ============================================================================
+
+struct bench_create_entities
+{
+	struct w_arena arena;
+	struct w_string_table string_table;
+	struct w_ecs_world world;
+};
+
+UBENCH_F_SETUP(bench_create_entities)
+{
+	w_arena_init(&ubench_fixture->arena, BENCH_ARENA_SIZE);
+	w_string_table_init(&ubench_fixture->string_table, &ubench_fixture->arena, 4096, 4096, w_hashmap_hash_str);
+	w_ecs_world_init(&ubench_fixture->world, &ubench_fixture->string_table, &ubench_fixture->arena);
+}
+
+UBENCH_F_TEARDOWN(bench_create_entities)
+{
+	w_ecs_world_free(&ubench_fixture->world);
+	w_string_table_free(&ubench_fixture->string_table);
+	w_arena_free(&ubench_fixture->arena);
+}
+
+UBENCH_F(bench_create_entities, world_unbuffered)
+{
+	ubench_fixture->world.buffering_enabled = false;
+	for (int i = 0; i < 10000; i++)
+	{
+		w_entity_id e = w_ecs_request_entity(&ubench_fixture->world);
+		UBENCH_DO_NOTHING(&e);
+	}
+}
+
+UBENCH_F(bench_create_entities, direct_registry)
+{
+	struct w_entity_registry *reg = &ubench_fixture->world.entities;
+	for (int i = 0; i < 10000; i++)
+	{
+		w_entity_id e = w_entity_request(reg);
+		UBENCH_DO_NOTHING(&e);
+	}
+}
+
+
+// ============================================================================
+// create named entities: world API vs direct registry
+// ============================================================================
+
+struct bench_create_named_entities
+{
+	struct w_arena arena;
+	struct w_string_table string_table;
+	struct w_ecs_world world;
+	char name_buf[32];
+};
+
+UBENCH_F_SETUP(bench_create_named_entities)
+{
+	w_arena_init(&ubench_fixture->arena, BENCH_ARENA_SIZE);
+	w_string_table_init(&ubench_fixture->string_table, &ubench_fixture->arena, 131072, 131072, w_hashmap_hash_str);
+	w_ecs_world_init(&ubench_fixture->world, &ubench_fixture->string_table, &ubench_fixture->arena);
+}
+
+UBENCH_F_TEARDOWN(bench_create_named_entities)
+{
+	w_ecs_world_free(&ubench_fixture->world);
+	w_string_table_free(&ubench_fixture->string_table);
+	w_arena_free(&ubench_fixture->arena);
+}
+
+UBENCH_F(bench_create_named_entities, world_buffered)
+{
+	ubench_fixture->world.buffering_enabled = true;
+	for (int i = 0; i < 10000; i++)
+	{
+		snprintf(ubench_fixture->name_buf, sizeof(ubench_fixture->name_buf), "Entity_%d", i);
+		w_entity_id e = w_ecs_request_entity_with_name(&ubench_fixture->world, ubench_fixture->name_buf);
+		UBENCH_DO_NOTHING(&e);
+	}
+	w_command_buffer_flush(&ubench_fixture->world.command_buffer);
+}
+
+UBENCH_F(bench_create_named_entities, world_unbuffered)
+{
+	ubench_fixture->world.buffering_enabled = false;
+	for (int i = 0; i < 10000; i++)
+	{
+		snprintf(ubench_fixture->name_buf, sizeof(ubench_fixture->name_buf), "Entity_%d", i);
+		w_entity_id e = w_ecs_request_entity_with_name(&ubench_fixture->world, ubench_fixture->name_buf);
+		UBENCH_DO_NOTHING(&e);
+	}
+}
+
+UBENCH_F(bench_create_named_entities, direct_registry)
+{
+	struct w_entity_registry *reg = &ubench_fixture->world.entities;
+	for (int i = 0; i < 10000; i++)
+	{
+		snprintf(ubench_fixture->name_buf, sizeof(ubench_fixture->name_buf), "Entity_%d", i);
+		w_entity_id existing = w_entity_lookup_by_name(reg, ubench_fixture->name_buf);
+		if (existing != W_ENTITY_INVALID)
+		{
+			UBENCH_DO_NOTHING(&existing);
+			continue;
+		}
+		w_entity_id e = w_entity_request(reg);
+		w_entity_set_name(reg, e, ubench_fixture->name_buf);
+		UBENCH_DO_NOTHING(&e);
+	}
+}
+
+
+// ============================================================================
+// return entities: world API vs direct registry
+// ============================================================================
+
+struct bench_return_entities
+{
+	struct w_arena arena;
+	struct w_string_table string_table;
+	struct w_ecs_world world;
+	w_entity_id *entities;
+};
+
+UBENCH_F_SETUP(bench_return_entities)
+{
+	w_arena_init(&ubench_fixture->arena, BENCH_ARENA_SIZE);
+	w_string_table_init(&ubench_fixture->string_table, &ubench_fixture->arena, 4096, 4096, w_hashmap_hash_str);
+	w_ecs_world_init(&ubench_fixture->world, &ubench_fixture->string_table, &ubench_fixture->arena);
+
+	// pre-create 10k entities to return
+	ubench_fixture->entities = malloc(10000 * sizeof(w_entity_id));
+	ubench_fixture->world.buffering_enabled = false;
+	for (int i = 0; i < 10000; i++)
+	{
+		ubench_fixture->entities[i] = w_ecs_request_entity(&ubench_fixture->world);
+	}
+}
+
+UBENCH_F_TEARDOWN(bench_return_entities)
+{
+	free(ubench_fixture->entities);
+	w_ecs_world_free(&ubench_fixture->world);
+	w_string_table_free(&ubench_fixture->string_table);
+	w_arena_free(&ubench_fixture->arena);
+}
+
+UBENCH_F(bench_return_entities, world_buffered)
+{
+	ubench_fixture->world.buffering_enabled = true;
+	for (int i = 0; i < 10000; i++)
+	{
+		w_ecs_return_entity(&ubench_fixture->world, ubench_fixture->entities[i]);
+	}
+	w_command_buffer_flush(&ubench_fixture->world.command_buffer);
+}
+
+UBENCH_F(bench_return_entities, world_unbuffered)
+{
+	ubench_fixture->world.buffering_enabled = false;
+	for (int i = 0; i < 10000; i++)
+	{
+		w_ecs_return_entity(&ubench_fixture->world, ubench_fixture->entities[i]);
+	}
+}
+
+UBENCH_F(bench_return_entities, direct_registry)
+{
+	struct w_entity_registry *reg = &ubench_fixture->world.entities;
+	for (int i = 0; i < 10000; i++)
+	{
+		w_entity_return(reg, ubench_fixture->entities[i]);
+	}
+}
+
+
+// ============================================================================
+// set components: world API vs direct registry
+// ============================================================================
+
+struct bench_component_16b
+{
+	uint64_t a;
+	uint64_t b;
+};
+
+struct bench_set_components
+{
+	struct w_arena arena;
+	struct w_string_table string_table;
+	struct w_ecs_world world;
+	w_entity_id type_id;
+	struct bench_component_16b data;
+};
+
+UBENCH_F_SETUP(bench_set_components)
+{
+	w_arena_init(&ubench_fixture->arena, BENCH_ARENA_SIZE);
+	w_string_table_init(&ubench_fixture->string_table, &ubench_fixture->arena, 4096, 4096, w_hashmap_hash_str);
+	w_ecs_world_init(&ubench_fixture->world, &ubench_fixture->string_table, &ubench_fixture->arena);
+
+	// pre-create component type
+	ubench_fixture->world.buffering_enabled = false;
+	ubench_fixture->type_id = w_ecs_request_entity(&ubench_fixture->world);
+	ubench_fixture->data.a = 123;
+	ubench_fixture->data.b = 456;
+}
+
+UBENCH_F_TEARDOWN(bench_set_components)
+{
+	w_ecs_world_free(&ubench_fixture->world);
+	w_string_table_free(&ubench_fixture->string_table);
+	w_arena_free(&ubench_fixture->arena);
+}
+
+UBENCH_F(bench_set_components, world_buffered)
+{
+	ubench_fixture->world.buffering_enabled = true;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		void *p = w_ecs_set_component_(&ubench_fixture->world, 0, ubench_fixture->type_id, i, &ubench_fixture->data, sizeof(ubench_fixture->data));
+		UBENCH_DO_NOTHING(p);
+	}
+	w_command_buffer_flush(&ubench_fixture->world.command_buffer);
+}
+
+UBENCH_F(bench_set_components, world_unbuffered)
+{
+	ubench_fixture->world.buffering_enabled = false;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		void *p = w_ecs_set_component_(&ubench_fixture->world, 0, ubench_fixture->type_id, i, &ubench_fixture->data, sizeof(ubench_fixture->data));
+		UBENCH_DO_NOTHING(p);
+	}
+}
+
+UBENCH_F(bench_set_components, direct_registry)
+{
+	struct w_component_registry *reg = &ubench_fixture->world.components;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		void *p = w_component_set_(reg, 0, ubench_fixture->type_id, i, &ubench_fixture->data, sizeof(ubench_fixture->data));
+		UBENCH_DO_NOTHING(p);
+	}
+}
+
+
+// ============================================================================
+// remove components: world API vs direct registry
+// ============================================================================
+
+struct bench_remove_components
+{
+	struct w_arena arena;
+	struct w_string_table string_table;
+	struct w_ecs_world world;
+	w_entity_id type_id;
+	struct bench_component_16b data;
+};
+
+UBENCH_F_SETUP(bench_remove_components)
+{
+	w_arena_init(&ubench_fixture->arena, BENCH_ARENA_SIZE);
+	w_string_table_init(&ubench_fixture->string_table, &ubench_fixture->arena, 4096, 4096, w_hashmap_hash_str);
+	w_ecs_world_init(&ubench_fixture->world, &ubench_fixture->string_table, &ubench_fixture->arena);
+
+	// pre-create component type and set 10k components
+	ubench_fixture->world.buffering_enabled = false;
+	ubench_fixture->type_id = w_ecs_request_entity(&ubench_fixture->world);
+	ubench_fixture->data.a = 123;
+	ubench_fixture->data.b = 456;
+
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		w_ecs_set_component_(&ubench_fixture->world, 0, ubench_fixture->type_id, i, &ubench_fixture->data, sizeof(ubench_fixture->data));
+	}
+}
+
+UBENCH_F_TEARDOWN(bench_remove_components)
+{
+	w_ecs_world_free(&ubench_fixture->world);
+	w_string_table_free(&ubench_fixture->string_table);
+	w_arena_free(&ubench_fixture->arena);
+}
+
+UBENCH_F(bench_remove_components, world_buffered)
+{
+	ubench_fixture->world.buffering_enabled = true;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		w_ecs_remove_component_(&ubench_fixture->world, ubench_fixture->type_id, i);
+	}
+	w_command_buffer_flush(&ubench_fixture->world.command_buffer);
+}
+
+UBENCH_F(bench_remove_components, world_unbuffered)
+{
+	ubench_fixture->world.buffering_enabled = false;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		w_ecs_remove_component_(&ubench_fixture->world, ubench_fixture->type_id, i);
+	}
+}
+
+UBENCH_F(bench_remove_components, direct_registry)
+{
+	struct w_component_registry *reg = &ubench_fixture->world.components;
+	for (w_entity_id i = 0; i < 10000; i++)
+	{
+		w_component_remove_(reg, ubench_fixture->type_id, i);
+	}
+}
+
+
 UBENCH_MAIN();
