7b15c24d049fc7894750b173f5737948c254bf1e
ElGatoPanzon <gato@elgatopanzon.io>
2026-03-07 11:17:01 -0600

    unified header whisker.h

---
+++ src/whisker.h
@@ -0,0 +1,48 @@
+/**
+ * @author      : ElGatoPanzon (contact@elgatopanzon.io)
+ * @file        : whisker
+ * @created     : Saturday Mar 07, 2026 11:09:45 CST
+ * @description : 
+ */
+
+// core utils
+#include "whisker_std.h"
+#include "whisker_debug.h"
+#include "whisker_macros.h"
+#include "whisker_random.h"
+#include "whisker_time.h"
+
+// memory
+#include "whisker_memory.h"
+#include "whisker_arena.h"
+
+// data structures
+#include "whisker_array.h"
+#include "whisker_hashmap.h"
+#include "whisker_sparse_bitset.h"
+#include "whisker_string_table.h"
+
+// hashing
+#include "whisker_hash_fnv1a.h"
+#include "whisker_hash_xxhash64.h"
+
+// ecs core tools
+#include "whisker_command_buffer.h"
+#include "whisker_hook_registry.h"
+#include "whisker_scheduler.h"
+
+// ecs modules
+#include "whisker_entity_registry.h"
+#include "whisker_component_registry.h"
+#include "whisker_system_registry.h"
+#include "whisker_query_registry.h"
+#include "whisker_query_iterator.h"
+#include "whisker_ecs_world.h"
+
+#ifndef WHISKER_H
+#define WHISKER_H
+
+
+
+#endif /* WHISKER_H */
+
