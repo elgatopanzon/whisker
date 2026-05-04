cf83417459c450bfd2f1dfaa2f9b46cc0c80b4c4
ElGatoPanzon <contact@elgatopanzon.io>
2026-05-03 19:58:30 -0600

    rendering: shapes: rectangle tris and lines + cube tris and lines

--- src/modules/rendering/whisker_rendering_shapes.h
+++ src/modules/rendering/whisker_rendering_shapes.h
@@ -11,6 +11,67 @@
 #ifndef WHISKER_RENDERING_SHAPES_H
 #define WHISKER_RENDERING_SHAPES_H
 
+// verts for shapes XZ native
+
+// rectangle
+static const w_vec3 w_shape_rect_tris[] = {
+    {0, 0, 0}, {0, 0, 1}, {1, 0, 0},
+    {1, 0, 0}, {0, 0, 1}, {1, 0, 1},
+};
+#define W_SHAPE_RECT_TRIS_LEN (sizeof(w_shape_rect_tris) / sizeof(*w_shape_rect_tris))
+
+// rectangle outline (4 edges, 8 verts)
+static const w_vec3 w_shape_rect_lines[] = {
+    {0, 0, 0}, {1, 0, 0},  // bottom
+    {1, 0, 0}, {1, 0, 1},  // right
+    {1, 0, 1}, {0, 0, 1},  // top
+    {0, 0, 1}, {0, 0, 0},  // left
+};
+#define W_SHAPE_RECT_LINES_LEN (sizeof(w_shape_rect_lines) / sizeof(*w_shape_rect_lines))
+
+// cube triangles (centered, -1 to 1 range, 36 verts)
+static const w_vec3 w_shape_cube_tris[] = {
+    // front
+    {-1, -1,  1}, { 1, -1,  1}, { 1,  1,  1},
+    {-1, -1,  1}, { 1,  1,  1}, {-1,  1,  1},
+    // back
+    { 1, -1, -1}, {-1, -1, -1}, {-1,  1, -1},
+    { 1, -1, -1}, {-1,  1, -1}, { 1,  1, -1},
+    // top
+    {-1,  1, -1}, {-1,  1,  1}, { 1,  1,  1},
+    {-1,  1, -1}, { 1,  1,  1}, { 1,  1, -1},
+    // bottom
+    {-1, -1, -1}, { 1, -1, -1}, { 1, -1,  1},
+    {-1, -1, -1}, { 1, -1,  1}, {-1, -1,  1},
+    // right
+    { 1, -1, -1}, { 1,  1, -1}, { 1,  1,  1},
+    { 1, -1, -1}, { 1,  1,  1}, { 1, -1,  1},
+    // left
+    {-1, -1,  1}, {-1,  1,  1}, {-1,  1, -1},
+    {-1, -1,  1}, {-1,  1, -1}, {-1, -1, -1},
+};
+#define W_SHAPE_CUBE_TRIS_LEN (sizeof(w_shape_cube_tris) / sizeof(*w_shape_cube_tris))
+
+// cube outline (12 edges, 24 verts)
+static const w_vec3 w_shape_cube_lines[] = {
+    // front face
+    {-1, -1,  1}, { 1, -1,  1},
+    { 1, -1,  1}, { 1,  1,  1},
+    { 1,  1,  1}, {-1,  1,  1},
+    {-1,  1,  1}, {-1, -1,  1},
+    // back face
+    {-1, -1, -1}, { 1, -1, -1},
+    { 1, -1, -1}, { 1,  1, -1},
+    { 1,  1, -1}, {-1,  1, -1},
+    {-1,  1, -1}, {-1, -1, -1},
+    // connecting edges
+    {-1, -1,  1}, {-1, -1, -1},
+    { 1, -1,  1}, { 1, -1, -1},
+    { 1,  1,  1}, { 1,  1, -1},
+    {-1,  1,  1}, {-1,  1, -1},
+};
+#define W_SHAPE_CUBE_LINES_LEN (sizeof(w_shape_cube_lines) / sizeof(*w_shape_cube_lines))
+
 /**************************
 *  shape tag components  *
 **************************/
